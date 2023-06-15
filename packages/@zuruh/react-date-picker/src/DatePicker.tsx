6d0cc145fb5707eff1adb7545b86dc8fba933ad0
Zuruuh <ziadi.mail.pro@gmail.com>
2023-06-15 13:05:34 +0200

    wip

--- packages/@zuruh/react-date-picker/src/DatePicker.tsx
+++ packages/@zuruh/react-date-picker/src/DatePicker.tsx
@@ -1,8 +1,6 @@
 import { type FC } from 'react';
 
-export interface DatePickerProps {
-  onSelect: (date: Date) => void;
-}
+export interface DatePickerProps {}
 
 export const DatePicker: FC<DatePickerProps> = () => {
   return <div data-testid="date-picker">Hello, World!</div>;
